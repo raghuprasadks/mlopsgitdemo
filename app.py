1ea134df7ff6db9f42109048b748c0aaf924f851
raghuprasadks <prasadraghuks@gmail.com>
2024-06-15 06:09:05 +0530

    updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "deployment on azure"
+    return "deployment on azure using github actions"
 
 if __name__ == '__main__':
     app.run()
